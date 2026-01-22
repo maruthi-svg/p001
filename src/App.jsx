1a218495b3d7e6ee3a40cb6ec6e892f58ee807b6
maruthi-svg <maruthimuthyala611@gmail.com>
2026-01-22 08:39:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return(
+    <div className='app'>
+      <h1>Name- maruthi </h1>
+        <p>Program- AI&DS </p>
+        ID NUM - 250008012
+    
+    </div>
+  );
+}
+export default App;
